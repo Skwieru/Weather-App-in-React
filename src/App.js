9fccb4f1c0770c08edd4f5578c6cede0dd9d029b
Skwieru <mskwieru@gmail.com>
2022-05-23 12:53:16 +0200

    https://github.com/Skwieru/weather-app-react.git

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 function App() {
+  const Map = ReactMapboxGl({
+    accessToken:
+      "pk.eyJ1Ijoic2t3aWVydSIsImEiOiJja3hnZjd4aHUxcno4Mm9wZGpqdHozaGllIn0.h8jnpRMJ1lcOJH1gHlmOeg",
+  });
+
+  const [cityName, setCityName] = useState(" ");
+
+  const handleInput = (e) => {
+    console.log(cityName);
+    setCityName(e.target.value);
+  };
+
+  const fetchWeather = async () => {
+    //   if (input.value === "") return;
+    //   const cityName = input.value;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form action="">
+        <label>Write a city name</label>
+        <input type="text" value={cityName} onChange={handleInput} />
+      </form>
+      <Map
+        style="mapbox://styles/mapbox/streets-v11"
+        containerStyle={{
+          height: "80vh",
+          width: "80vw",
+          margin: "0 auto",
+        }}
+      >
+        <Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}>
+          <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+        </Layer>
+      </Map>
+      ;
+    </>
   );
 }
 
